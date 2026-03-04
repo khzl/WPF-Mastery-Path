053feb64d05079c3ebe163c1a8acb3d04a129778
khzl <madkhzlk45@gmail.com>
2026-03-03 18:08:25 -0800

    Complete Day 8

---
+++ WPF.BootCamps/Views/Day-8/FormLogin.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPF.BootCamps.Views.Day_8
+{
+    /// <summary>
+    /// Interaction logic for FormLogin.xaml
+    /// </summary>
+    public partial class FormLogin : Window
+    {
+        public FormLogin()
+        {
+            InitializeComponent();
+        }
+    }
+}
