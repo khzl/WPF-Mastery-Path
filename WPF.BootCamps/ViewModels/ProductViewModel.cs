a597c9a40b1712d50743878f9924c9e09e7f2ea0
khzl <madkhzlk45@gmail.com>
2026-03-14 17:28:15 -0700

    Complete Day-14

---
+++ WPF.BootCamps/ViewModels/ProductViewModel.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Text;
+using System.Windows.Input;
+using WPF.BootCamps.Helpers;
+using WPF.BootCamps.Models;
+
+namespace WPF.BootCamps.ViewModels
+{
+    public class ProductViewModel : INotifyPropertyChanged
+    {
+        // List -> ObservableCollection 
+        public ObservableCollection<md_Product_PL>? Products { get; set; } = new ObservableCollection<md_Product_PL>()
+        {
+            new md_Product_PL
+            {
+                Name = "Laptop",
+                Price = 1200
+            },
+            new md_Product_PL
+            {
+                Name = "Phone",
+                Price = 800
+            },
+            new md_Product_PL
+            {
+                Name = "Keyboard",
+                Price = 100
+            }
+        };
+
+        // private field 
+        private md_Product_PL? _selectedProduct;
+
+        // Property
+        public md_Product_PL? SelectedProduct
+        {
+            get => _selectedProduct;
+            set
+            {
+                _selectedProduct = value;
+                OnPropertyChanged(nameof(SelectedProduct));
+            }
+        }
+
+        // Event Handler
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public void OnPropertyChanged(string name) =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+
+        // Command 
+        public ICommand? DeleteCommand { get; } // ReadOnly
+
+        // Constructor
+        public ProductViewModel()
+        {
+            DeleteCommand = new RelayCommand(DeleteProduct);
+        }
+
+        // Delete Product
+        private void DeleteProduct(object? obj)
+        {
+            if (obj is md_Product_PL product)
+                Products?.Remove(product);
+        }
+    }
+}
---
+++ WPF.BootCamps/Views/Day-14/ProductList.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using WPF.BootCamps.ViewModels;
+
+namespace WPF.BootCamps.Views.Day_14
+{
+    /// <summary>
+    /// Interaction logic for ProductList.xaml
+    /// </summary>
+    public partial class ProductList : Window
+    {
+        public ProductList()
+        {
+            InitializeComponent();
+            // link ProductViewModel in this view using DataContext
+            DataContext = new ProductViewModel();
+        }
+    }
+}
